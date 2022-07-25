0bddfbe50eb51d26d7bd912bc3eb6bd07ee508f6
Yen kuang Chen <yenkuangchen@Yens-MacBook-Pro.local>
2022-07-25 08:21:54 +0800

    adjusting state and re-render issue

--- src/api/psyService.js
+++ src/api/psyService.js
@@ -4,12 +4,16 @@ class psyService {
     serverAddress = "";
     testAddress = "http://localhost:8844/";
 
-    saveStroopEffect(subject, information, result) {
-        return axios.post(this.testAddress + "stroop_effect/save/" + {"subject":subject, "information":information, "result":result});
+    saveStroopEffectSubject(subject, information) {
+        return axios.post(this.testAddress + "stroop_effect/save_subject", {"subject":subject, "information":information});
     }
 
     getStroopEffect(subject) {
         return axios.get(this.testAddress + `stroop_effect/get?subject=${subject}`);
+    }
+
+    updateStroopEffect(subject, reponse) {
+        
     }
 }
 
--- src/pages/psychology.jsx
+++ src/pages/psychology.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import NavBar from '../nav/nav';
 import PsyMainContent from '../components/psyPageComponents/psyMainContent';
 import HeaderPsy from '../components/headers/headerPsy';
-import psyService from '../api/psyService';
-//import HeaderPsy from '../components/headers/headerPsy';
 
 const Psychology = () => {
-    psyService.getStroopEffect("yk")
-    .then( res => {
-        console.log(res)
-    } )
+
     return ( 
         <React.Fragment>
             <HeaderPsy/>
